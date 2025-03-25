7785a1d0478540aad203434c369b646b8840748c
BigColossal <jeremyperez1409@gmail.com>
2025-03-24 20:25:12 -0400

    Finished product, complete report now given to user on any document

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-from stats import get_word_amount
+from stats import get_word_amount, get_character_amounts, sort_characters
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    filepath = sys.argv[1]
 
 def get_book_text(filepath):
     with open(filepath, "r") as file:
         return file.read()
 
 def main():
-    words = get_word_amount(get_book_text("books/frankenstein.txt"))
-    print(f"{words} words found in the document")
+
+    book = get_book_text(filepath)
+
+    words = get_word_amount(book)
+
+    characters = get_character_amounts(book)
+
+    sorted_list = sort_characters(characters)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {words} total words")
+    print("--------- Character Count -------")
+    for dict in sorted_list:
+        if dict["character"].isalpha():
+            print(f"{dict["character"]}: {dict["num"]}")
+
 
 main()
 
--- stats.py
+++ stats.py
@@ -1,3 +1,30 @@
 def get_word_amount(string):
     words = string.split()
     return len(words)
+
+def get_character_amounts(string):
+    characters = {}
+    words = string.split()
+    for word in words:
+        for letter in word:
+            letter = letter.lower()
+            if letter in characters:
+                characters[letter] += 1
+            else:
+                characters[letter] = 1
+
+    return characters
+
+def sort_characters(char_dict):
+    sorted_list = []
+    for char in char_dict:
+        new_char_dict = {}
+        new_char_dict["character"] = char
+        new_char_dict["num"] = char_dict[char]
+        sorted_list.append(new_char_dict)
+
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+def sort_on(dict):
+    return dict["num"]
